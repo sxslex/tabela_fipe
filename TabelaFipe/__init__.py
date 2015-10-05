86607eee21b2cb37cef8c79b78e555950cef7911
sxslex <sx.slex@gmail.com>
2015-10-05 20:53:35 -0300

    add implementation and db file

---
+++ TabelaFipe/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Alexandre Villela (SleX) <https://github.com/sxslex/sxtools/>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+__version__ = '0.0.1'
+__description__ = 'TabelaFipe - Vehicle information: Manufacturer, Model, ' + \
+    'Vehicle, versions and prices.'
+__keywords__ = 'simple access library of data from the Table Fipe' + \
+    'Vehicle information, Manufacturer, Model, Vehicle, versions and prices'
+__author__ = 'Alexandre Villela (SleX)'
+__author_email__ = 'sx.slex@gmail.com'
+
+from tabela_fipe import TabelaFipe
---
+++ TabelaFipe/tabela_fipe.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Alexandre Villela (SleX) <https://github.com/sxslex/sxtools/>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# TabelaFipe
+#   sx.slex@gmail.com
+import os
+import sqlite_single
+PATH_DB = os.path.join(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    ),
+    'tabela_fipe.db'
+)
+
+
+class TabelaFipe(object):
+
+    def __init__(self):
+        self.db = sqlite_single.SqliteSingle(PATH_DB)
+
+    def get_by_codefipe(self, codefipe):
+        resp = self.db.query(
+            sql='''
+                select
+                    mod.mod_cod,
+                    mod.mod_codigofipe,
+                    tiv.tiv_text,
+                    mar.mar_text,
+                    mod.mod_text
+                from mod_modelo mod
+                left join mar_marca mar on mod.mar_cod=mar.mar_cod
+                left join tiv_tipoveiculo tiv on mar.tiv_cod=tiv.tiv_cod
+                where trim(mod.mod_codigofipe) = ?
+            ''',
+            params=[codefipe]
+        )
+        if not resp:
+            return None
+        dresp = dict(
+            mod_cod=resp[0][0],
+            codigo_fipe=resp[0][1],
+            tipo=resp[0][2],
+            marca=resp[0][3],
+            modelo=resp[0][4],
+            versoes=[]
+        )
+        resp_ano = self.db.query(
+            sql='''
+            select ver_anomodelo, ver_combustivel, ver_valor
+            from ver_versao
+            where mod_cod=?
+            ''',
+            params=[dresp['mod_cod']]
+        )
+        dresp['versoes'] = [
+            dict(
+                ano=ver[0],
+                combustivel=ver[1],
+                valor=ver[2]
+            ) for ver in resp_ano
+        ]
+        return dresp
